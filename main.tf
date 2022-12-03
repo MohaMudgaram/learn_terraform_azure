dc5a39c01d456290daf4efbbebeec1ab46b3ca32
Sriram058 <Sriram058@users.noreply.github.com>
2022-12-03 00:41:57 -0500

    Update to Add few more Resources

--- main.tf
+++ main.tf
@@ -14,10 +14,38 @@ provider "azurerm" {
   features {}
 }
 
+# Decalaring Local Variables
+locals {
+  resource_group_name = "learn-33322297-0052-48ef-83ba-44ae9a25e46e"
+  location            = "westus"
+}
+
 # Create a virtual network within the resource group
 resource "azurerm_virtual_network" "vnet" {
   name                = "example-network"
-  resource_group_name = "learn-8b0b8118-65b4-4d6d-8ef0-d6443cc0fccc"
-  location            = "West US"
+  resource_group_name = local.resource_group_name
+  location            = local.location
   address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "storageaccount1203221230"
+  resource_group_name      = local.resource_group_name
+  location                 = local.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "example" {
+  name                  = "content"
+  storage_account_name  = azurerm_storage_account.example.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_blob" "example" {
+  name                   = "az_test.txt"
+  storage_account_name   = azurerm_storage_account.example.name
+  storage_container_name = azurerm_storage_container.example.name
+  type                   = "Block"
+  source                 = "test.txt"
 }
